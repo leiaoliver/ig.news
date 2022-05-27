6a1a1c9c66f30e3415c9de59154f30555a059755
Leia <leiaoliver388@gmail.com>
2022-05-26 23:20:56 -0300

    create components Buttons and Home

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from "../../node_modules/next/head";
+import { SubscribeButton } from "../components/SubscribeButton/index";
+
+import styles from "./home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> word.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
