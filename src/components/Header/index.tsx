87bd09ff68d6e1896d9430e8ed896525d8ccb12f
Leia <leiaoliver388@gmail.com>
2022-07-01 13:32:06 -0300

    feat - consuming github api

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,26 @@
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+import {ActiveLink} from '../ActiveLink';
+
 import {SignInButton} from '../SignInButton/index';
+
 import styles from './styles.module.scss';
 
 export function Header() {
+
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
-
         <SignInButton />
       </div>
     </header>
