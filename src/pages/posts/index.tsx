24eac69c17366cb42c8415e480121f268bae9feb
Leia <leiaoliver388@gmail.com>
2022-07-01 13:35:27 -0300

    feat - creating pages post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,25 @@
+import {GetStaticProps} from 'next';
 import Head from 'next/head';
+import * as prismic from '@prismicio/client';
+import {RichText} from 'prismic-dom';
+import Link from 'next/link';
+
+import getPrismicClient from '../../services/prismic';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  exerpt: string;
+  updateAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({posts}: PostsProps) {
   return (
     <>
       <Head>
@@ -11,29 +28,51 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>21 de março de 2021</time>
-            <strong>Creating Monorepo with & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-            </p>
-          </a>
-          <a href="#">
-            <time>21 de março de 2021</time>
-            <strong>Creating Monorepo with & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-            </p>
-          </a>
-          <a href="#">
-            <time>21 de março de 2021</time>
-            <strong>Creating Monorepo with & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn how to create a Monorepo to manage
-            </p>
-          </a>
+          {posts.map(post => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updateAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.exerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismicClient = getPrismicClient();
+
+  const response = await prismicClient.get({
+    predicates: prismic.predicate.at('document.type', 'post'),
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  });
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      exerpt:
+        post.data.content.find(content => content.type === 'paragraph')?.text ??
+        '',
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        },
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
