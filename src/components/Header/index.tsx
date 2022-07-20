5fe5ef59add932ebd96b488f6b32777a5e49dc29
Leia <leiaoliver388@gmail.com>
2022-07-20 15:14:41 -0300

    fix - header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,6 @@ import {SignInButton} from '../SignInButton/index';
 import styles from './styles.module.scss';
 
 export function Header() {
-
-
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
